a4d3e5624e539ae4a2ea6f72b11f550e9dd76dc7
niesz <sznie778@163.com>
2018-10-09 17:45:18 +0800

    继续努力

--- src/cn/java/tetris/Tetromino.java
+++ src/cn/java/tetris/Tetromino.java
@@ -2,19 +2,40 @@ package cn.java.tetris;
 
 public class Tetromino {
 	
-	private Cell cells[];
+	/**
+	 *每个形状四个格子
+	 *protected修饰符，以被继承类调用，也可以被自己类里的函数调用，但不能被其他的类调用
+	 */
+	protected Cell[] cells = new Cell[4];
 	
 	
 	
-	public void moveDown() {
+	/**随机生成一个四格方块*/
+	public static Tetromino random_one() {
+		Tetromino t = null;
+		int num = (int)(Math.random()*7);
+		switch(num) {
+		case 0:t = new I();break;
+		case 1:t = new J();break;
+		case 2:t = new L();break;
+		case 3:t = new O();break;
+		case 4:t = new S();break;
+		case 5:t = new T();break;
+		case 6:t = new Z();break;	
+		}
+		return t;
+	}
+	
+	
+	public void move_down() {
 		
 	}
 	
-	public void moveLeft() {
+	public void move_left() {
 		
 	}
 	
-	public void moveRight() {
+	public void move_right() {
 		
 	}
 	
